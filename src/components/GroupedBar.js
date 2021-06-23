621896712c8cf01c86641492535ab8e36117634c
amace <contact.alexandremace@gmail.com>
2021-06-23 08:23:25 +0200

    add first diagram

---
+++ src/components/GroupedBar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+    labels: ['Maisons individuelles', 'Logements collectifs', 'Bureau, enseignement', 'Hôtel'],
+    datasets: [
+        {
+            label: '# of Red Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(255, 99, 132)',
+        },
+        {
+            label: '# of Blue Votes',
+            data: [2, 3, 20, 5, 1, 4],
+            backgroundColor: 'rgb(54, 162, 235)',
+        },
+        {
+            label: '# of Green Votes',
+            data: [3, 10, 13, 15, 22, 30],
+            backgroundColor: 'rgb(75, 192, 192)',
+        },
+    ],
+};
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                stacked: true,
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+        xAxes: [
+            {
+                stacked: true,
+            },
+        ],
+    },
+};
+
+const GroupedBar = () => (
+    <>
+        <div className='header'>
+            <h1 className='title'>Grouped Bar Chart</h1>
+            <div className='links'>
+                <a
+                    className='btn btn-gh'
+                    href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/GroupedBar.js'
+                >
+                    Github Source
+                </a>
+            </div>
+        </div>
+        <Bar data={data} options={options} />
+    </>
+);
+
+export default GroupedBar;
