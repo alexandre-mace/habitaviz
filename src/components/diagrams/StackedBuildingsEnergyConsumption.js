bd990551957047cc1c702a00e8ee5f87865650c3
alexandre-mace <contact.alexandremace@gmail.com>
2021-08-22 16:30:56 +0200

    Add playing feature

--- src/components/diagrams/StackedBuildingsEnergyConsumption.js
+++ src/components/diagrams/StackedBuildingsEnergyConsumption.js
@@ -2,12 +2,17 @@ import GroupedBar from "./../GroupedBar";
 import data from "./../../data";
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import InputLabel from '@material-ui/core/InputLabel';
 import Switch from '@material-ui/core/Switch';
-import {FormControlLabel} from "@material-ui/core/index";
+import {Button, FormControlLabel} from "@material-ui/core/index";
 import domainToStackedChartJsAdapter from "../../adapter/domainToStackedChartJsAdapter";
 import Period from "../utilities/Period";
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+
+let periodsToPlay = ['2015', '2016'];
+let periodPlayingHasStoppedWhileTimeOut = false;
 
 function StackedBuildingsEnergyConsumption() {
     const [valueUnity, setValueUnity] = useState('kWhep/m2.an');
@@ -15,13 +20,33 @@ function StackedBuildingsEnergyConsumption() {
     const [endYear, setEndYear] = useState('2015');
     const [fluid, setFluid] = useState('Gaz');
     const [averageValues, setAverageValues] = useState(false);
+    const [playPeriod, setPlayPeriod] = useState(false);
+
+    useEffect(() => {
+        if (playPeriod !== false) {
+            setTimeout(() => {
+                periodsToPlay = (periodsToPlay.filter(period => period !== periodsToPlay[0]))
+                if (periodsToPlay.length === 0 || periodPlayingHasStoppedWhileTimeOut) {
+                    periodPlayingHasStoppedWhileTimeOut = false
+                    setPlayPeriod(false);
+                    periodsToPlay = ['2015', '2016'];
+                    return;
+                }
+
+                setStartYear(periodsToPlay[0]);
+                setEndYear(periodsToPlay[0]);
+                setPlayPeriod(periodsToPlay[0]);
+            }, 1500)
+        }
+    }, [playPeriod])
 
     return (
         <div>
             <div className='header'>
                 <h1 className='title'>Consommation énergétique des bâtiments : diagramme 1 (min et max "stackés")</h1>
             </div>
-            <div style={{ display: 'flex' }}>
+
+            <div style={{ display: 'flex', alignItems: 'center' }}>
                 <div style={{display: 'inline-block'}}>
                     <InputLabel id="fluid-label">Fluide</InputLabel>
                     <Select
@@ -39,7 +64,7 @@ function StackedBuildingsEnergyConsumption() {
                     </Select>
                 </div>
                 <div style={{marginRight: '2rem'}}/>
-                <Period startYear={startYear} setStartYear={setStartYear} endYear={endYear} setEndYear={setEndYear}/>
+                <Period startYear={startYear} setStartYear={setStartYear} endYear={endYear} setEndYear={setEndYear} setPlayPeriod={setPlayPeriod}/>
                 <div style={{marginRight: '2rem'}}/>
                 <div style={{display: 'inline-block'}}>
                     <InputLabel id="value-unity-label">Unité</InputLabel>
@@ -60,6 +85,26 @@ function StackedBuildingsEnergyConsumption() {
                         label="Valeur moyenne"
                     />
                 </div>
+                {playPeriod &&
+                <Button
+                    style={{marginLeft: '2rem', height: 40}}
+                    variant="contained"
+                    color="primary" onClick={() => {
+                        periodPlayingHasStoppedWhileTimeOut = true;
+                        setPlayPeriod(false)
+                    }}
+                    endIcon={<PauseCircleOutlineIcon />}
+                >Stop</Button>
+                }
+                {!playPeriod &&
+                <Button
+                    style={{marginLeft: '2rem', height: 40}}
+                    variant="contained"
+                    color="primary" onClick={() => setPlayPeriod(periodsToPlay[0])}
+                    endIcon={<PlayCircleOutlineIcon />}
+                >Play</Button>
+                }
+                {(playPeriod && typeof periodsToPlay[0] !== 'undefined') && <h2 style={{marginLeft: '2rem', marginTop: 0, marginBottom: 0}}>{periodsToPlay[0]}</h2>}
             </div>
             <GroupedBar data={domainToStackedChartJsAdapter(data, startYear, endYear, averageValues, fluid)}/>
         </div>
